15021133e6c5d5eb0afc10cd7fd746bceeb73142
nwysbwwq319 <1557439389@qq.com>
2024-10-20 16:34:35 +0800

    Modify the second graph, the original graph satisfies link_constraint, and the modified graph satisfies the common constraints of link_constraint and p

--- plot_BG2VN.m
+++ plot_BG2VN.m
@@ -1,34 +1,30 @@
-% 生成BG2VN的图d
-function plot_BG2VN(x,N,link_constraint,MU_set)
+% Generate a graph of BG2VN
+function plot_BG2VN(x,N,adjacency_matrix,MU_set)
 
-%% 普通节点+关键节点
+%% graph of k sets of two-dimensional Gaussian distributed samples
 figure;
 scatter(x(:, 1), x(:, 2), 10, 'filled', 'MarkerFaceColor', 'b' ,'DisplayName', 'Ordinary Samples');
 hold on;
 scatter(MU_set(:, 1), MU_set(:, 2), 30, 'filled', 'MarkerFaceColor', 'r','DisplayName', 'Mean');
-% 设置图形属性
+
 xlabel('X-coordinate','FontSize', 15, 'FontWeight', 'bold');
 ylabel('Y-coordinate','FontSize', 15, 'FontWeight', 'bold');
-axis equal; % 设置坐标轴比例相等
+axis equal; % Setting the axes to scale equally
 hold off;
 legend show; 
 
 
-%% 点+边
+%% graph of BG2VN
 figure;
 hold on;
 
-% 绘制所有点
+% Plot all points
 %scatter(x(:, 1), x(:, 2), 10, 'filled', 'MarkerFaceColor', 'b');
 
-% 循环遍历所有点并连接距离小于 1 的点
 for i = 1:N-1
-    for j = i+1:N % 避免重复计算
-        % 计算两点之间的距离
-        dis=distance(x(i,1),x(i,2),x(j,1),x(j,2));
-        
-        % 如果距离小于 link_constraint，则画线
-        if dis < link_constraint
+    for j = i+1:N % Avoiding double counting 
+        % Draw a line if edges exist in adjacency_matrix
+        if adjacency_matrix(i,j)==1
             plot([x(i, 1), x(j, 1)], [x(i, 2), x(j, 2)], 'g');
         end
     end
@@ -37,7 +33,6 @@ scatter(x(:, 1), x(:, 2), 10, 'filled', 'MarkerFaceColor', 'b' ,'DisplayName', '
 hold on;
 scatter(MU_set(:, 1), MU_set(:, 2), 30, 'filled', 'MarkerFaceColor', 'r','DisplayName', 'Mean');
 
-axis equal; % 设置坐标轴比例相等
-%grid on; % 添加网格
+axis equal; % Setting the axes to scale equally
 axis off;
 hold off;
